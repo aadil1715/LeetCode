6f50665b284f99fbbe4f6e7a2a41889b4911251a
Aadil Shaikh <aadilshaikh862@gmail.com>
2022-01-01 14:10:06 -0400

    Condensed the code

--- container-with-most-water/container-with-most-water.java
+++ container-with-most-water/container-with-most-water.java
@@ -4,10 +4,9 @@ class Solution {
         int r = height.length - 1;
         int areac = 1;
         int area = 0;
-        if(height.length == 2){
-            return height[0]>height[1] ? height[1] : height[0];
-        }
         while(l<r){
+            // Breadth = r - l;
+            // Height = Math.min(height[r],height[l]);
             areac = (r-l) * Math.min(height[r],height[l]);
             area = Math.max(area,areac);
             if(height[l]<height[r]){
