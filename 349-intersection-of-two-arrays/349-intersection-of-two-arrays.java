5644b25ca979496395f0a69f9290ebbc5b4de216
Aadil Shaikh <aadilshaikh862@gmail.com>
2022-02-14 00:59:47 -0400

    Time: 1 ms (99.39%), Space: 42.1 MB (28.34%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -1,56 +1,24 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        List<Integer> li = new ArrayList<Integer>();
+        HashSet<Integer> set = new HashSet<Integer>();
+        HashSet<Integer> intersection = new HashSet<>();
         
-        if(n<m){
-            int[] temp = nums2;
-            Arrays.sort(temp);
-            for(int i=0;i<n;i++){
-                if(binary_search(nums1[i],temp)){
-                    if(!li.contains(nums1[i])){
-                    li.add(nums1[i]);
-                    }
-                }
-            }
+        for (int n : nums1){
+            set.add(n);
         }
-        else{
-            int[] temp = nums1;
-            Arrays.sort(temp);
-            for(int i=0;i<m;i++){
-                if(binary_search(nums2[i],temp)){
-                    if(!li.contains(nums2[i])){
-                    li.add(nums2[i]);
-                    }
-                }
+        
+        for (int n : nums2){
+            if(set.contains(n)){
+                intersection.add(n);
             }
         }
         
-        System.out.println(li);
-        int op[] = new int[li.size()];
-        for(int i=0;i<li.size();i++){
-            op[i] = li.get(i);
+        int op[] = new int[intersection.size()];
+        int j = 0;
+        for (Integer i : intersection){
+            op[j++] = i;
         }
         
         return op;
-    }
-    
-    public boolean binary_search(int n,int[] nums){
-        int left=0;
-        int right=nums.length-1;
-        while(left<=right){
-            int mid = (left + right)/2;
-            if(nums[mid]==n){
-                return true;
-            }
-            else if(n<nums[mid]){
-                right = mid -1;
-            }
-            else{
-                left = mid + 1;
-            }
-        }
-        return false;
     }
 }
